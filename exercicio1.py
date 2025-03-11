dbb13dfc9872c591fa8e6e0b18176a51b9b16b4f
luanapatrio <luana.patricio@cs.cruzeirodosul.edu.br>
2025-03-11 20:55:54 -0300

    Exercício 1
    Área e perímetro de um triângulo.

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+
+lado1 = float(input("Digite o valor do primeiro lado do triângulo: "))
+lado2 = float(input("Digite o valor do segundo lado do triângulo: "))
+
+print("Perímetro:", 2 * (lado1 + lado2))
+print("Área:", lado1 * lado2)
