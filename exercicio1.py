bba625cd95b59e3836761ea2806eba0ab1829d8e
luanapatrio <luana.patricio@cs.cruzeirodosul.edu.br>
2025-03-11 21:14:36 -0300

    Exercício2
    Salário e acréscimo de 5%

--- exercicio1.py
+++ exercicio1.py
@@ -4,3 +4,5 @@ lado2 = float(input("Digite o valor do segundo lado do triângulo: "))
 
 print("Perímetro:", 2 * (lado1 + lado2))
 print("Área:", lado1 * lado2)
+
+#Float - para números que possam ter casa decimal com resultados precisos.
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+salario = float(input("Digite o valor do seu salário: "))
+
+comissao = salario * 1.05
+
+print(f"Seu salário com comissão é: R$ {comissao:.2f}")
