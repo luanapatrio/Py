0b0c2cdf78a6dc0e68cc3083e1748f72bc401756
luanapatrio <luana.patricio@cs.cruzeirodosul.edu.br>
2025-03-22 20:07:15 -0300

    att

--- aula 2/exercicio1.py
+++ aula 2/exercicio1.py
@@ -1,12 +1,7 @@
-def calcular(h, menor, maior):
-    volume = (h/3) * (maior**2 + menor**2 + (maior**2 * menor**2) ** 0.5)
-    return volume
-    
-
 h = float(input("Digite o valor da altura da pirâmide: "))
 menor = float(input("Digite o valor da base menor da pirâmide: "))
 maior = float(input("Digite o valor da base maior da pirâmide: "))
 
-volume = calcular(h, menor, maior)
+volume = (h/3) * (maior**2 + menor**2 + (maior**2 * menor**2) ** 0.5)
 
 print(f"O volume do tronco da pirâmide é: {volume:.2f}")
